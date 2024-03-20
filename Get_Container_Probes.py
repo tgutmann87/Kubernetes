cb7ffa3e6516ff1b7e9e8526d9e79b3ad0f1a189
tgutmann87 <31452444+tgutmann87@users.noreply.github.com>
2024-03-20 14:27:32 -0400

    Add files via upload

--- Get_Container_Probes.py
+++ Get_Container_Probes.py
@@ -11,31 +11,34 @@ with open('Container_Probe_Information.csv', 'w', newline='') as file:
     kubectl_output = json.loads(kubectl_output)
 
     for pod in kubectl_output['items']:
-        if pod['status']['phase'] == 'Running':
-            for container in pod['spec']['containers']:
-                row = [pod['metadata']['namespace'], pod['metadata']['name'], container['name']]
-                
-                try:
-                    row.extend(container["livenessProbe"].values())
-                    
+        try:
+            if pod['status']['phase'] == 'Running' and pod['metadata']['labels']['product-line'] == 'candp':
+                for container in pod['spec']['containers']:
+                    row = [pod['metadata']['namespace'], pod['metadata']['name'], container['name']]
+
                     try:
-                        row.extend(container["readinessProbe"].values())
+                        row.extend(container["livenessProbe"].values())
+                        
+                        try:
+                            row.extend(container["readinessProbe"].values())
+                            
+                        except:
+                            for x in range(0,6):
+                                row.append(' ')
+                                
+                        file.writerow(row)
                         
                     except:
                         for x in range(0,6):
                             row.append(' ')
-                            
-                    file.writerow(row)
-                    
-                except:
-                    for x in range(0,6):
-                        row.append(' ')
-                    
-                    try:
-                        row.extend(container["readinessProbe"].values())
-                        file.writerow(row)
-                       
-                    except:
-                        row = ''
+                        
+                        try:
+                            row.extend(container["readinessProbe"].values())
+                            file.writerow(row)
+                           
+                        except:
+                            row = ''
+        except:
+            print('No Product Line')
             
     
